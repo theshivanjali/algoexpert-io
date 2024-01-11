4968c54732923991c3cad7febf91a08987730785
Shivanjali Chaurasia <shivanjali012@gmail.com>
2024-01-11 20:16:23 +0530

    2 Down! 198 To Go!

---
+++ python/two-number-sum.py
@@ -0,0 +1,24 @@
+"""
+    Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. If any two numbers in the input array sum up to the target sum, the function should return them in an array, in any order. If no two numbers sum up to the target sum, the function should return an empty array.
+    Note that the target sum has to be obtained by summing two different integers in the array; you can't add a single integer to itself in order to obtain the target sum.
+    You can assume that there will be at most one pair of numbers summing up to the target sum
+"""
+
+array = [3, 5, -4, 8, 11, 1, -1, 6]
+targetSum = 10
+
+
+def twoNumberSum(array, targetSum):
+    # Write your code here.
+    exist = {}
+    for num in array:
+        x = targetSum - num
+        print(x)
+        if x in exist:
+            return [num, x]
+        exist[num] = True
+        print(exist)
+    return []
+
+
+print(twoNumberSum(array, targetSum))
